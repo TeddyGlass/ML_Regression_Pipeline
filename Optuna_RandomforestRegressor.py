c759838da2973d0acb7b7caa09f52c284dbcb73a
kuro3210 <kota.kurosaki0226@gmail.com>
2019-12-26 16:55:28 +0900

    Optuna_RF_Regression

--- Optuna_RandomforestRegressor.py
+++ Optuna_RandomforestRegressor.py
@@ -1,3 +1,4 @@
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.ensemble import RandomForestRegressor
 import optuna
@@ -14,32 +15,34 @@ def objective_rf(trial):
     min_samples_split=trial.suggest_int('min_samples_split', 2, X_train.shape[0]//2)
     min_weight_fraction_leaf=trial.suggest_uniform('min_weight_fraction_leaf', 0, 0.5)
     n_estimators=trial.suggest_int('n_estimators', 100, 2000)
-    # model
-    model = RandomForestRegressor(bootstrap=True,
-                                  ccp_alpha=0.0,
-                                  criterion='mse',
-                                  max_depth=max_depth,
-                                  max_features=max_features,
-                                  max_leaf_nodes=max_leaf_nodes,
-                                  max_samples=max_samples,
-                                  min_impurity_decrease=min_impurity_decrease,
-                                  min_impurity_split=min_impurity_split,
-                                  min_samples_leaf=min_samples_leaf,
-                                  min_samples_split=min_samples_split,
-                                  min_weight_fraction_leaf=min_weight_fraction_leaf,
-                                  n_estimators=n_estimators,
-                                  n_jobs=-1,
-                                  oob_score=False,
-                                  random_state=0,
-                                  verbose=1,
-                                  warm_start=False
-                                 )
-    # crosss validation
-    kf = KFold(n_splits=2, shuffle=True, random_state=0)
-    scores = cross_val_score(model, X_train, y_train, cv=kf, scoring='neg_mean_squared_error')
-    score_mean = scores.mean()
-    
-    return score_mean
-
-study = optuna.create_study()
-study.optimize(objective_rf, n_trials=100, n_jobs=-1)
+    # cross validation
+    scoring_list = []
+    for train_index, valid_index in KFold(n_splits=2).split(X_train, y_train):
+        # model training
+        model = RandomForestRegressor(bootstrap=True,
+                                      ccp_alpha=0.0,
+                                      criterion='mse',
+                                      max_depth=max_depth,
+                                      max_features=max_features,
+                                      max_leaf_nodes=max_leaf_nodes,
+                                      max_samples=max_samples,
+                                      min_impurity_decrease=min_impurity_decrease,
+                                      min_impurity_split=min_impurity_split,
+                                      min_samples_leaf=min_samples_leaf,
+                                      min_samples_split=min_samples_split,
+                                      min_weight_fraction_leaf=min_weight_fraction_leaf,
+                                      n_estimators=n_estimators,
+                                      n_jobs=-1,
+                                      oob_score=False,
+                                      random_state=0,
+                                      verbose=0,
+                                      warm_start=False
+                                     )
+        model.fit(X_train[train_index], y_train[train_index])
+        # scoring
+        y_true = y_train[valid_index]
+        y_pred = model.predict(X_train[valid_index])
+        rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+        scoring_list.append(rmse)
+        
+    return np.mean(scoring_list)
