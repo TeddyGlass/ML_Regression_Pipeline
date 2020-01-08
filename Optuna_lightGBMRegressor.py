23b23ea3e1d654ffbf5287f587f60f7cc553bcd9
TeddyGlass <kota.kurosaki0226@gmail.com>
2020-01-08 17:18:18 +0900

    add xgb optimization code

--- Optuna_lightGBMRegressor.py
+++ Optuna_lightGBMRegressor.py
@@ -1,14 +1,14 @@
 import numpy as np
-from sklearn.datasets import load_digits
+from sklearn.datasets import load_boston
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold, cross_val_score, train_test_split
 from lightgbm import LGBMRegressor
 import optuna
 
 # data sets
-digits = load_digits()
-X = digits['data']
-y = digits['target']
+boston = load_boston()
+X = boston['data']
+y = boston['target']
 
 # data split
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1217, test_size=0.2)
