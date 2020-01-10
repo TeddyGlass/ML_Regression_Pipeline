fa635fcfcc3ee84995232c9552a33ea77e33b383
TeddyGlass <kota.kurosaki0226@gmail.com>
2020-01-10 15:51:19 +0900

    add nn optimization code

--- Optuna_Neuralnet.py
+++ Optuna_Neuralnet.py
@@ -45,56 +45,56 @@ def create_model(input_dropout, hidden_dropout, n_layers, batch_norm, units, act
 
 # Objective function
 def obj(trial):
-# parameter space
-input_dropout = trial.suggest_uniform('input_dropout', 0, 0.3)
-hidden_dropout = trial.suggest_uniform('hidden_dropout', 0, 0.3)
-n_layers = trial.suggest_int('n_layers', 1, 5)
-units = int(trial.suggest_discrete_uniform('units', 6, 26, 1))
-batch_norm = trial.suggest_categorical('batch_norm', ['act', 'no'])
-activation = trial.suggest_categorical('activation', ['prelu', 'relu'])
-batch_size = int(trial.suggest_discrete_uniform('batch_size', 32, 128, 32))
-# model building
-model = create_model(
-    input_dropout=input_dropout,
-    hidden_dropout=hidden_dropout,
-    n_layers=n_layers,
-    units=units,
-    activation=activation,
-    batch_norm=batch_norm
-)
-# optimizer
-optimizer = Adam(lr=0.01, beta_1=0.9, beta_2=0.999, decay=0.)
-# Compile
-model.compile(
-    loss='mean_squared_error',
-    optimizer=optimizer,
-    metrics=['mae']
-)
-# Epoch and Early stopping
-early_stopping = EarlyStopping(patience=20, restore_best_weights=True)
-# train
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1217, test_size=0.1)
-scoring_list = []
-for train_index, valid_index in KFold(n_splits=5, random_state=0).split(X_train, y_train):
-    scaler = StandardScaler()
-    x_tr = scaler.fit_transform(X_train[train_index])
-    x_va = scaler.fit_transform(X_train[valid_index])
-    y_tr = y_train[train_index]
-    y_va = y_train[valid_index]
-    history = model.fit(
-        x_tr, y_tr,
-        epochs=200,
-        batch_size=batch_size, verbose=1,
-        validation_data=(x_va, y_va),
-        callbacks=[early_stopping]
+    # parameter space
+    input_dropout = trial.suggest_uniform('input_dropout', 0, 0.3)
+    hidden_dropout = trial.suggest_uniform('hidden_dropout', 0, 0.3)
+    n_layers = trial.suggest_int('n_layers', 1, 5)
+    units = int(trial.suggest_discrete_uniform('units', 6, 26, 1))
+    batch_norm = trial.suggest_categorical('batch_norm', ['act', 'no'])
+    activation = trial.suggest_categorical('activation', ['prelu', 'relu'])
+    batch_size = int(trial.suggest_discrete_uniform('batch_size', 32, 128, 32))
+    # model building
+    model = create_model(
+        input_dropout=input_dropout,
+        hidden_dropout=hidden_dropout,
+        n_layers=n_layers,
+        units=units,
+        activation=activation,
+        batch_norm=batch_norm
     )
-    # prediction
-    y_true = y_va
-    y_pred = model.predict(x_va).flatten()
-    rmse = np.sqrt(mean_squared_error(y_true, y_pred))
-    scoring_list.append(rmse)
-    
-return np.mean(scoring_list)  
+    # optimizer
+    optimizer = Adam(lr=0.01, beta_1=0.9, beta_2=0.999, decay=0.)
+    # Compile
+    model.compile(
+        loss='mean_squared_error',
+        optimizer=optimizer,
+        metrics=['mae']
+    )
+    # Epoch and Early stopping
+    early_stopping = EarlyStopping(patience=20, restore_best_weights=True)
+    # train
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1217, test_size=0.1)
+    scoring_list = []
+    for train_index, valid_index in KFold(n_splits=5, random_state=0).split(X_train, y_train):
+        scaler = StandardScaler()
+        x_tr = scaler.fit_transform(X_train[train_index])
+        x_va = scaler.fit_transform(X_train[valid_index])
+        y_tr = y_train[train_index]
+        y_va = y_train[valid_index]
+        history = model.fit(
+            x_tr, y_tr,
+            epochs=200,
+            batch_size=batch_size, verbose=1,
+            validation_data=(x_va, y_va),
+            callbacks=[early_stopping]
+        )
+        # prediction
+        y_true = y_va
+        y_pred = model.predict(x_va).flatten()
+        rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+        scoring_list.append(rmse)
+        
+    return np.mean(scoring_list)  
 
 
 def main():
