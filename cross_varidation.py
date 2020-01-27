659873fa3b237bca25fd53919dbcb895a547c7a8
kuro3210 <kota.kurosaki0226@gmail.com>
2020-01-27 12:17:55 +0900

    :tada: crocssval

---
+++ cross_varidation.py
@@ -0,0 +1,11 @@
+import numpy as np
+from sklearn.model_selection import KFold
+
+def generate_cv_index(X, y, folds, random_state):
+    train_index = [train_index for train_index, valid_index in KFold(n_splits=folds, random_state=random_state).split(X, y)]
+    valid_index = [valid_index for train_index, valid_index in KFold(n_splits=folds, random_state=random_state).split(X, y)]
+    key = ['train_index', 'valid_index']
+    value = [train_index, valid_index]
+    cv_dictionary = {}
+    cv_dictionary.update(zip(key, value))
+    return cv_dictionary
